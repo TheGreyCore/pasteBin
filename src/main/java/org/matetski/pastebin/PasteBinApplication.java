55100b040ec296f53fa2791520e5ee876e020aae
Dmitri Matetski <dmitri.matetski@gmail.com>
2024-03-06 11:26:20 +0200

    Revert "Revert "Init commit""
    This reverts commit 1045cb4d7ea84868100e9a227b237a29b1ffb906.

---
+++ src/main/java/org/matetski/pastebin/PasteBinApplication.java
@@ -0,0 +1,16 @@
+package org.matetski.pastebin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasteBinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasteBinApplication.class, args);
+	}
+
+}
+
+
+
